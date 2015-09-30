8ca71caeb7fc277ebffbc775970d4861eabc6d71
nalotjr <nalotjr@gmail.com>
2015-09-30 08:56:05 +0300

    some changes for configuration file works

--- src/utilites/dao/ConfigDAOFactory.java
+++ src/utilites/dao/ConfigDAOFactory.java
@@ -1,14 +1,27 @@
 package utilites.dao;
 
 /**
- * Factory method creates and returns object of ConfigDAO class which realize methods of SettingsDAO interface for
+ * Factory method creates and returns object of ConfigXMLDAO class which realize methods of SettingsDAO interface for
  * configuration file.
  * Created by AT on 25.08.2015.
  */
 public class ConfigDAOFactory extends SettingsDAOFactory {
 
+    private SettingsDAO settingsDAO;
+
+    ConfigDAOFactory(int typeOfSource) {
+
+        switch (typeOfSource) {
+            case CONFIG:
+                settingsDAO = new ConfigSerializableDAO();
+                break;
+            default:
+                break;
+        }
+    }
+
     @Override
     public SettingsDAO manageSettingsDAO() {
-        return new ConfigDAO();
+        return settingsDAO;
     }
 }
