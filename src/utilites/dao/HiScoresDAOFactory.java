f0e9610371a0927070f99b4a66442abae1fe420c
nalotjr <nalotjr@gmail.com>
2015-09-30 08:57:51 +0300

    changed realization of hiscore file

--- src/utilites/dao/HiScoresDAOFactory.java
+++ src/utilites/dao/HiScoresDAOFactory.java
@@ -1,15 +1,27 @@
 package utilites.dao;
 
 /**
- * Factory method creates and returns object of HiScoresDAO class which realize methods of SettingsDAO interface for
+ * Factory method creates and returns instance of class which realize SettingsDAO interface for selected type of
  * high scores data file.
  * Created by AT on 25.08.2015.
  */
 public class HiScoresDAOFactory extends SettingsDAOFactory {
 
+    private SettingsDAO settingsDAO;
+
+    HiScoresDAOFactory(int typeOfSource) {
+
+        switch (typeOfSource) {
+            case HISCORES:
+                settingsDAO = new HiScoresSerializableDAO();
+                break;
+            default:
+                break;
+        }
+    }
+
     @Override
     public SettingsDAO manageSettingsDAO() {
-
-        return new HiScoresDAO();
+        return settingsDAO;
     }
 }
